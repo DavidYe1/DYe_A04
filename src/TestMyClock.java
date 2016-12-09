3b5b84625d55819053a48de5557a03327ae7382f
David Ye <david.ye@live.de>
2016-12-09 13:08:46 +0100

    comments

--- src/TestMyClock.java
+++ src/TestMyClock.java
@@ -8,6 +8,11 @@ import unittests.MyClock;
 public class TestMyClock {
     MyClock c = new MyClock();
     @Test
+    /**
+     * test für den konstruktor
+     *
+     * keine fehler
+     */
     public void testKonstruktor(){
         new MyClock();
         String t = c.showTime();
@@ -15,7 +20,11 @@ public class TestMyClock {
         Assert.assertEquals("0:0:0",t);
     }
     @Test
-
+    /**
+     * zeit setzen
+     *
+     * keine fehler
+     */
     public void testSetTime(){
         c.setTime(1,2,3);
         String t = c.showTime();
@@ -36,12 +45,22 @@ public class TestMyClock {
         Assert.assertEquals("0:0:0",t);
     }
     @Test
+    /**
+     * zeit in string umwanteln
+     *
+     * keine fehler
+     */
     public void testshowTime(){
         String t = new MyClock().showTime();
 
         Assert.assertEquals("0:0:0",t);
     }
     @Test
+    /**
+     * zeit in int array zurück geben
+     *
+     * keine fehler
+     */
     public void testGetTime(){
         c.setTime(1,2,3);
         int[] a = {1,2,3};
@@ -49,22 +68,47 @@ public class TestMyClock {
         Assert.assertArrayEquals(a,c.getTime());
     }
     @Test(expected = IllegalArgumentException.class)
+    /**
+     * üngültiger wert für stunde
+     *
+     * wirft wie vorhergesehen eine ecxeption
+     */
     public void testNegativWert(){
         c.setTime(-1,0,0);
     }
     @Test(expected = IllegalArgumentException.class)
+    /**
+     * ungültiger wert
+     *
+     * wirft wie vorhergesehen eine ecxeption
+     */
     public void testOvertimeStunde(){
         c.setTime(25,0,0);
     }
     @Test(expected = IllegalArgumentException.class)
+    /**
+     * ungültiger wert für minute
+     *
+     * wirft wie vorhergesehen eine ecxeption
+     */
     public void testOvertimeMinute(){
         c.setTime(0,90,0);
     }
     @Test(expected = IllegalArgumentException.class)
+    /**
+     * ungültiger wert sekunde
+     *
+     * wirft wie vorhergesehen eine ecxeption
+     */
     public void testOvertimeSecond(){
         c.setTime(0,0,90);
     }
     @Test
+    /**
+     * grenzwert test für sec
+     *
+     * erhöht minute richtig
+     */
     public void testTickMinute(){
         c.setTime(0,0,59);
         c.tick();
@@ -73,6 +117,11 @@ public class TestMyClock {
         Assert.assertEquals("0:1:0",t);
     }
     @Test
+    /**
+     * grenzwert test für min
+     *
+     * erhöt stunde richtig
+     */
     public void testTickStunde(){
         c.setTime(0,59,59);
         c.tick();
